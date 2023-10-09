d081e1e62f2ae26f677af6ce6ece311b434e4299
노정완 <jeongwan.noh@gmail.com>
2023-10-09 14:11:12 +0900

    going through ch8

---
+++ src/main/java/me/jeongwannoh/springbootdeveloper/config/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package me.jeongwannoh.springbootdeveloper.config;
+
+import lombok.RequiredArgsConstructor;
+import me.jeongwannoh.springbootdeveloper.service.UserDetailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebSecurityConfig {
+
+    private final UserDetailService userService;
+
+    //스프링 기능 비활성화
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                .requestMatchers(toH2Console())
+                .requestMatchers("/static/**");
+    }
+}
